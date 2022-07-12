fa23deeeaeaaaadd743f66a5ad8a0e1895fcebd6
CeciAmaro <amaro.ceci.10@gmail.com>
2022-07-11 20:21:10 -0500

    Creada aplicacion Places
    Agregados modelo Places y serializador

---
+++ lugares_seguros/places/serializers.py
@@ -0,0 +1,8 @@
+from dataclasses import field, fields
+from rest_framework import serializers
+from .models import Place
+
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Place
+        fields = '__all__'
